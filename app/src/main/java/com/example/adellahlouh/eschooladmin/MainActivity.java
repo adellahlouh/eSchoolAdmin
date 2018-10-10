665fe344a4fbe37a13f9310b8e6e6e9b964ceb99
adellahlouh <adel.bashar@gmail.com>
2018-10-10 22:53:56 +0300

    add comment

--- app/src/main/java/com/example/adellahlouh/eschooladmin/MainActivity.java
+++ app/src/main/java/com/example/adellahlouh/eschooladmin/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Hello Adel
+
         btn_login = findViewById(R.id.btn_login);
         edt_email = findViewById(R.id.edt_Email);
         edt_password = findViewById(R.id.edt_Pass);
