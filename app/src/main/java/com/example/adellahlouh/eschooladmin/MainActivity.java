b8e5ee6bd5f34aef33be88f1f7b33f49b536a192
adellahlouh <adel.bashar@gmail.com>
2018-10-11 01:17:43 +0300

    new comment

--- app/src/main/java/com/example/adellahlouh/eschooladmin/MainActivity.java
+++ app/src/main/java/com/example/adellahlouh/eschooladmin/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.adellahlouh.eschooladmin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //jdjdjdjdjjd
+        //jdjdjdjdjjdghg
 
         btn_login = findViewById(R.id.btn_login);
         edt_email = findViewById(R.id.edt_Email);
