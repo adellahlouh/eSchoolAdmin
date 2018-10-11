4d2c264890980efdfb84c1b183ed996cc613e08d
adellahlouh <adel.bashar@gmail.com>
2018-10-11 18:00:57 +0300

    connect firebase auth
    Signed-off-by: adellahlouh <adel.bashar@gmail.com>

--- app/src/main/java/com/example/adellahlouh/eschooladmin/MainActivity.java
+++ app/src/main/java/com/example/adellahlouh/eschooladmin/MainActivity.java
@@ -16,9 +16,6 @@ public class MainActivity extends AppCompatActivity {
     EditText edt_email, edt_password;
     TextView txt_check;
 
-
-// hjghjgdjghkgfjfjhghfgjvjhj
-    //gyhgjhghjghjghjgjghj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
